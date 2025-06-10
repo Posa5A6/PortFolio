b527f8acdc6f0408c0ce02c43d313ea4ad52e531
Posa Narendra <171539501+Posa5A6@users.noreply.github.com>
2025-06-10 06:45:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
